9e1152ef99a0da4f6c3c2d1b4ea8e1b49716dbfe
sumaks-xr <suman.singhxr@gmail.com>
2019-10-29 23:49:35 +0530

    refined FPS controll added movement  gravity jump on character controller

--- SimpeBasketBall/Assets/ProjectFiles/Scripts/PlayerMovement.cs
+++ SimpeBasketBall/Assets/ProjectFiles/Scripts/PlayerMovement.cs
@@ -1,24 +1,61 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    //data variable related to movement
     public CharacterController controller;
     public float speed = 10f;
+    public float gravity = -9.81f;
+    [SerializeField] private Vector3 velocity;
 
-    // Update is called once per frame
+    //related to ground check
+    public Transform groundCheck;
+    public float groundDistance = 0.3f;
+    public LayerMask groundMask;
+    bool isGrounded;
+
+    //related to Jump
+    public float jumpHeight =3f;
+    
     void Update()
     {
+        CheckIfGrounded();
         PlayerMove();
+        GravityPlayerFall();
+        PlayerJump();
     }
 
+    private void CheckIfGrounded()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);// groundDistance is check sphere's radius
+        if (isGrounded && velocity.y <0)
+        {
+            velocity.y = 0f;// value =-2 ac to brackeys
+        }
+    }
     private void PlayerMove()
     {
         float inputXmove = Input.GetAxis("Horizontal");
         float inputYmove = Input.GetAxis("Vertical");
-        Vector3 move = transform.right * inputXmove + transform.forward * inputYmove;
+        Vector3 directionMove = transform.right * inputXmove + transform.forward * inputYmove;
 
-        controller.Move(move * speed * Time.deltaTime);
+        controller.Move(directionMove * speed * Time.deltaTime);
+    }
+    private void GravityPlayerFall()
+    {
+        
+        velocity.y += gravity * Time.deltaTime;//v=u+at//velocity is a vector its y component is newton low applied
+
+        controller.Move((velocity*Time.deltaTime)/2);//change in displacement s = 1/2(at^2)//physics of freefall
+    }
+    private void PlayerJump()
+    {
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(-2 * gravity * jumpHeight);
+        }
     }
 }
