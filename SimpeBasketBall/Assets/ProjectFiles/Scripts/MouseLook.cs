f0ddb0c724ddf238f3e5bad55212ca5e620b9c5c
sumaks-xr <suman.singhxr@gmail.com>
2019-10-29 05:36:13 +0530

    added stairs and character controller and mouse look script and movement script

---
+++ SimpeBasketBall/Assets/ProjectFiles/Scripts/MouseLook.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    //Reference to attach in the unity editor
+    public Transform playerTarget;
+    public float mouseSensitivity = 100f;
+    float xAxisRotation = 0f;
+    
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        PlayerYaxisRotation();
+        PlayerXaxisRotation();
+    }
+
+    private void PlayerYaxisRotation()
+    {
+        float inputMouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        playerTarget.Rotate(Vector3.up * inputMouseX);
+    }
+    private void PlayerXaxisRotation()
+    {
+        float inputMouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xAxisRotation += inputMouseY;
+        xAxisRotation = Mathf.Clamp(xAxisRotation, -90, 90);
+        transform.localRotation = Quaternion.Euler(-xAxisRotation, 0f, 0f);
+    }
+    
+}
---
+++ SimpeBasketBall/Assets/ProjectFiles/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 10f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMove();
+    }
+
+    private void PlayerMove()
+    {
+        float inputXmove = Input.GetAxis("Horizontal");
+        float inputYmove = Input.GetAxis("Vertical");
+        Vector3 move = transform.right * inputXmove + transform.forward * inputYmove;
+
+        controller.Move(move * speed * Time.deltaTime);
+    }
+}
